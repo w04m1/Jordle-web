ef02804ccf78a93d59772395857c17ca2a9eeb5d
w04m1 <70951451+w04m1@users.noreply.github.com>
2024-03-06 21:46:57 +0100

    feat(Keyboard): :sparkles: create Keyboard v1
    create first version of Keyboard component

--- frontend/src/components/Keyboard.svelte
+++ frontend/src/components/Keyboard.svelte
@@ -1,6 +1,8 @@
 <script>
   import { currentGuess, guesses, gameStatus } from "../store.js";
 
+  const rows = ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"];
+
   function handleKeyClick(key) {
     if (key === "Enter") {
       // TODO: handle the enter key press
@@ -15,18 +17,56 @@
 </script>
 
 <div class="keyboard">
-  {#each "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("") as key}
-    <button on:click={() => handleKeyClick(key)}>{key}</button>
+  {#each rows as row}
+    <div class="row">
+      {#if row === "ZXCVBNM"}
+        <button class="function" on:click={() => handleKeyClick("Backspace")}
+          >&#x232B;</button
+        >
+      {/if}
+      {#each row.split("") as letter}
+        <button class="letter" on:click={() => handleKeyClick(letter)}
+          >{letter}</button
+        >
+      {/each}
+      {#if row === "ZXCVBNM"}
+        <button
+          class="function"
+          style="font-weight: bold"
+          on:click={() => handleKeyClick("Enter")}>&crarr;</button
+        >
+      {/if}
+    </div>
   {/each}
-  <button on:click={() => handleKeyClick("Enter")}>Enter</button>
-  <button on:click={() => handleKeyClick("Backspace")}>Backspace</button>
 </div>
 
 <style>
   .keyboard {
-    /* TODO: keyboard styles */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
-  button {
-    /* TODO: key styles */
+
+  .row {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+
+  .letter,
+  .function {
+    margin: 5px;
+    padding: 10px 15px;
+    flex-grow: 1;
+    min-width: 40px;
+    max-width: 60px;
+  }
+
+  @media (max-width: 590px) {
+    .letter,
+    .function {
+      padding: 5px 10px;
+      min-width: 20px;
+    }
   }
 </style>
